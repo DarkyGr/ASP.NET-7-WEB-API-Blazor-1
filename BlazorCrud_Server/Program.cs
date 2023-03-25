735953a481b49aa899d0c0d1d991fde03fe77115
Guillermo Riaño <guilledark17@gmail.com>
2023-03-25 16:50:47 -0600

    📦 Configuration: SQLString 2

--- BlazorCrud_Server/Program.cs
+++ BlazorCrud_Server/Program.cs
@@ -1,3 +1,6 @@
+using BlazorCrud_Server.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Adding dabatase context
+builder.Services.AddDbContext<DbBlazorwebapicrud1Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SQLString"));
+});
 
 var app = builder.Build();
 
