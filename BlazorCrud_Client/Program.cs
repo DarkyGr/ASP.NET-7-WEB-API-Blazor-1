c71add938ee01190a2eda96fa2051ef88c031ffc
Guillermo Riaño <guilledark17@gmail.com>
2023-03-31 10:38:04 -0600

    ✅ Adding Services: Program.cs

--- BlazorCrud_Client/Program.cs
+++ BlazorCrud_Client/Program.cs
@@ -2,10 +2,16 @@ using BlazorCrud_Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using BlazorCrud_Client.Services;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5297") });
+
+// Adding services
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 
 await builder.Build().RunAsync();
